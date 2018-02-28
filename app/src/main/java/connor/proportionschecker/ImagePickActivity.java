0ff298e6d703d116f356aa7cd7eef4ea13ca0a04
Connor Penrod <cfpvf9@mail.missouri.edu>
2018-02-27 20:26:14 -0600

    Added flipped video view in separate fragment

--- app/src/main/java/connor/proportionschecker/ImagePickActivity.java
+++ app/src/main/java/connor/proportionschecker/ImagePickActivity.java
@@ -1,5 +1,7 @@
 package connor.proportionschecker;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,10 +15,20 @@ public class ImagePickActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_pick);
 
-        final Button button = findViewById(R.id.quit_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Context imagePickContext = this;
+
+        final Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.i("Test", "test message");
+                Intent cameraIntent = new Intent(imagePickContext, FlippedCameraActivity.class);
+                startActivity(cameraIntent);
+            }
+        });
+
+        final Button quitButton = findViewById(R.id.quit_button);
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
             }
         });
     }
